473f83f2fd8e4aec8625872abd3fa3268a9203d4
Amitai Burstein <amitai@gizra.com>
2013-07-30 22:07:16 +0300

    work

--- app/scripts/directives/addToCart.js
+++ app/scripts/directives/addToCart.js
@@ -59,7 +59,7 @@ angular.module('AngularShoppingCartApp')
       templateUrl: 'views/cart-checkout.html',
       restrict: 'E',
       scope: {
-        'products': '='
+        'cart': '='
       }
     };
   });
